7b9a7cb4751d390d2d952ec4d92f4938b58997f9
yuda <yudhaprama@icloud.com>
2022-05-31 19:48:17 +0700

    Support null safety

--- lib/indonesia.dart
+++ lib/indonesia.dart
@@ -4,13 +4,13 @@
 library indonesia;
 
 /// Convert value to "Rp XX.XXX.XXX" as [String]
-String rupiah(value, {String separator='.', String trailing=''}) {
-	return "Rp " + value.toString().replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]}${separator}') + trailing;
+String rupiah(value, {String? separator='.', String? trailing=''}) {
+	return "Rp " + value.toString().replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]}${separator}') + trailing!;
 }
 
 /// Convert [DateTime] to Indonesia specific date
-String tanggal(DateTime date, {bool shortMonth=false}) {
-	return "${date.day} ${_convertToLocalMonth(date.month, shortMonth)} ${date.year}";
+String tanggal(DateTime date, {bool? shortMonth=false}) {
+	return "${date.day} ${_convertToLocalMonth(date.month, shortMonth!)} ${date.year}";
 }
 
 List _longMonth = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
